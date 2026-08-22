ebce24a1c12d97d0df4f14b627faac4531aa9e6f
Covru34 <mikolamihalcuk01@gmail.com>
2026-08-22 10:32:08 +0300

    add text to QR

--- React/Lessons 1. Begin React/my-app/src/pages/QRPage.js
+++ React/Lessons 1. Begin React/my-app/src/pages/QRPage.js
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import QRCode from "react-qr-code";
 
 const QRPage = () => {
+    const [input, setInput] = useState("");
+    const [qrValue, setQrValue] = useState("https://kherson.itstep.org/");
+
+    const handleGenerate = () => {
+        if (input.trim() !== "") {
+            setQrValue(input);
+        }
+    };
+
     return (
         <>
             <h1 className={"text-center"}>Генерація QR кодів</h1>
-            <QRCode value="https://www.google.com.ua/" />
+
+            <input
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                placeholder="Введіть URL"
+            />
+            <button onClick={handleGenerate}>Генерувати</button>
+
+            <QRCode value={qrValue} />
         </>
-    )
-}
+    );
+};
 
 export default QRPage;
