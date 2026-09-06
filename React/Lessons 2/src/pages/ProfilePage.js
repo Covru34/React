d6361f4661a26d52b114ee4ac8d7eb4669a393d7
Covru34 <mikolamihalcuk01@gmail.com>
2026-09-06 13:18:54 +0300

    add biblioteks zod and react-hook-from

--- React/Lessons 2/src/pages/ProfilePage.js
+++ React/Lessons 2/src/pages/ProfilePage.js
@@ -1,20 +1,101 @@
-import { z } from "zod"; //імпортуємо Zod для валідації даних
+import { z } from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod"; //імпортуємо Zod для валідації даних
 //zod - бібліотека для валідації даних на клієнті
 //react-hook-forms - для роботи з форами на клієнті
 
 //Робимо схему валіації даних
+// Схема валідації
 const profileSchema = z.object({
     firstName: z
-        .string() // людина має вводити текст - рядок
-        .min(2, "Ім'я має мати щонайменше 2 символи") // мін довжина символів
-        .max(50, "Ім'я занадто довге"), // максимум скількі символів
-})
+        .string()
+        .min(2, "Ім'я має мати щонайменше 2 символи")
+        .max(50, "Ім'я занадто довге"),
+    lastName: z
+        .string()
+        .min(2, "Призвище має мати щонайменше 2 символи")
+        .max(50, "Призвище занадто довге"),
+    email: z.string().email("Некоректна email адреса")
+});
+
+//Поля, які будуть на формі
+const defaultValues = {
+    firstName: "",
+    lastName: "",
+    email: ""
+};
 
 const ProfilePage = () =>
 {
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors, isDirty },
+    } = useForm({
+        resolver: zodResolver(profileSchema),
+        defaultValues, //Це об'єкт, який описує нашу форму
+        mode: "onBlur",
+    }); // Налаштувати форму для роботи
+
+    const onSubmit = (data) => {
+        //Цей метод буде спрацьовувати, яколи дані у формі буть вірними
+        //Тобто валідні- через zod
+        console.log("Form Data:", data);
+    }
+
+    console.log("--errors--", errors.firstName, isDirty);
+
     return (
         <>
             <h1 className={"text-center"}>Профіль користувача</h1>
+
+            <div className="container mt-2">
+                {/*у нас react-hook-forms - буде обробляти подію відправки форми*/}
+                <form onSubmit={handleSubmit(onSubmit)} className="col-md-6 offset-md-3">
+                    {/* Ім'я */}
+                    <div className="mb-3">
+                        <label htmlFor="firstName" className="form-label">Ім'я</label>
+                        <input
+                            type="text"
+                            className={`form-control ${errors.firstName ? "is-invalid" : ""}`}
+                            id="firstName"
+                            placeholder="Іван"
+                            {...register("firstName")}
+                        />
+                        {errors.firstName && <div className="invalid-feedback">{errors.firstName.message}</div>}
+                    </div>
+
+                    {/* Призвище */}
+                    <div className="mb-3">
+                        <label htmlFor="lastName" className="form-label">Призвище</label>
+                        <input
+                            type="text"
+                            className={`form-control ${errors.firstName ? "is-invalid" : ""}`}
+                            id="lastName"
+                            placeholder="Хмельницький"
+                            {...register("lastName")}
+                        />
+                        {errors.lastName && <div className="invalid-feedback">{errors.lastName.message}</div>}
+                    </div>
+
+                    {/* Пошта */}
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Електронна адреса</label>
+                        <input
+                            type="text"
+                            className={`form-control ${errors.firstName ? "is-invalid" : ""}`}
+                            id="email"
+                            placeholder="pinkiepie@gmail.com"
+                            {...register("email")}
+                        />
+                        {errors.email && <div className="invalid-feedback">{errors.email.message}</div>}
+                    </div>
+
+                    <button type="submit" className="btn btn-success">Зберегти</button>
+                </form>
+            </div>
         </>
     )
 }
