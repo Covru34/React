3521b4bc7efbfe9f41d0afab517f974ff992f2d7
Covru34 <mikolamihalcuk01@gmail.com>
2026-09-13 09:59:48 +0300

    add bootstrap

---
+++ React/Lessons 1. Begin React/step-by-step/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import './App.css';
+
+function App() {
+  return (
+   <>
+     <h1 className={"text-center"}>Привіт козаки та козачки</h1>
+   </>
+  );
+}
+
+export default App;
